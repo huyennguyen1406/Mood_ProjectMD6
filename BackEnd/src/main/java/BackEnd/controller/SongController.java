a2ea380c7f1eeb0e3d10be8223cc915c1ed16d70
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-04-10 18:06:44 +0700

    create controller

---
+++ BackEnd/src/main/java/BackEnd/controller/SongController.java
@@ -0,0 +1,11 @@
+package BackEnd.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/home/song")
+public class SongController {
+}
